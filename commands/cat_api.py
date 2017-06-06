2b2698d03c179bbf50ce256aa27b9a5e71d892c3
deeshugupta <gupta.deeshu@gmail.com>
2017-06-07 00:53:23 +0530

    adding help documents

--- commands/cat_api.py
+++ commands/cat_api.py
@@ -12,6 +12,10 @@ def cat():
 @cat.command('indices', short_help='The indices command provides a cross-section of each index.')
 @click.option('--index', nargs=1, help=' A comma-separated list of index names to limit the returned information')
 def indices(index):
+    '''
+    The indices command provides a cross-section of each index. This information spans nodes.
+    We can tell quickly how many shards make up an index, the number of docs, deleted docs, primary store size, and total store size (all shards including replicas). All these exposed metrics come directly from Lucene APIs.
+    '''
     try:
         response = base.es.cat.indices(index)
         table = base.draw_table(response)
@@ -20,9 +24,10 @@ def indices(index):
     else:
         click.echo(table.draw())
 
-@cat.command()
+@cat.command('aliases', short_help='Information about current aliases')
 @click.option('--name', nargs=1)
 def aliases(name):
+    '''aliases shows information about currently configured aliases to indices including filter and routing infos.'''
     try:
         response = base.es.cat.aliases(name)
         if not response:
