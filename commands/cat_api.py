d3453c91f8c6b2c162fc69998dc4ad6018cd8c59
deeshugupta <gupta.deeshu@gmail.com>
2017-06-08 00:46:29 +0530

    Adding more commands to ES

--- commands/cat_api.py
+++ commands/cat_api.py
@@ -11,13 +11,15 @@ def cat():
 
 @cat.command('indices', short_help='The indices command provides a cross-section of each index.')
 @click.option('--index', nargs=1, help=' A comma-separated list of index names to limit the returned information')
-def indices(index):
+@click.option('-h', nargs=1, help=' A comma-separated list of column names')
+
+def indices(index,h):
     '''
     The indices command provides a cross-section of each index. This information spans nodes.
     We can tell quickly how many shards make up an index, the number of docs, deleted docs, primary store size, and total store size (all shards including replicas). All these exposed metrics come directly from Lucene APIs.
     '''
     try:
-        response = base.es.cat.indices(index)
+        response = base.es.cat.indices(index, h=h)
         table = base.draw_table(response)
     except Exception as e:
         click.echo(e)
@@ -25,11 +27,12 @@ def indices(index):
         click.echo(table.draw())
 
 @cat.command('aliases', short_help='Information about current aliases')
-@click.option('--name', nargs=1)
-def aliases(name):
+@click.option('--name', nargs=1, help='A comma-separated list of alias names to return')
+@click.option('-h', nargs=1, help=' A comma-separated list of column names')
+def aliases(name,h):
     '''aliases shows information about currently configured aliases to indices including filter and routing infos.'''
     try:
-        response = base.es.cat.aliases(name)
+        response = base.es.cat.aliases(name,h=h)
         if not response:
             click.echo("No aliases as of now")
             return
@@ -40,12 +43,13 @@ def aliases(name):
         click.echo(table.draw())
 
 
-@cat.command()
-@click.option('--node', nargs=1)
-@click.option('--format', default='mb')
-def allocation(node,format):
+@cat.command('allocation', short_help='Allocation provides a snapshot of how shards have located around the cluster and the state of disk usage')
+@click.option('--node', nargs=1, help='A comma-separated list of node IDs or names to limit the returned information')
+@click.option('-h', nargs=1, help=' A comma-separated list of column names')
+@click.option('--format', default='mb', help='The unit in which to display byte values, valid choices are: \'b\', \'k\', \'kb\', \'m\', \'mb\', \'g\', \'gb\', \'t\', \'tb\', \'p\', \'pb\'')
+def allocation(node,format,h):
     try:
-        response = base.es.cat.allocation(node_id=node,bytes=format)
+        response = base.es.cat.allocation(node_id=node,bytes=format, h=h)
         table = base.draw_table(response)
     except Exception as e:
         click.echo(e)
@@ -55,9 +59,10 @@ def allocation(node,format):
 
 @cat.command()
 @click.option('--index', nargs=1)
-def count(index):
+@click.option('-h', nargs=1, help=' A comma-separated list of column names')
+def count(index,h):
     try:
-        response = base.es.cat.count(index)
+        response = base.es.cat.count(index,h=h)
         table = base.draw_table(response)
     except Exception as e:
         click.echo(e)
@@ -67,9 +72,10 @@ def count(index):
 @cat.command()
 @click.option('--fields', nargs=1)
 @click.option('--format', default='mb')
-def fielddata(fields,format):
+@click.option('-h', nargs=1, help=' A comma-separated list of column names')
+def fielddata(fields,format,h):
     try:
-        response = base.es.cat.fielddata(fields=fields,bytes=format)
+        response = base.es.cat.fielddata(fields=fields,bytes=format,h=h)
         table = base.draw_table(response)
     except Exception as e:
         click.echo(e)
@@ -78,9 +84,52 @@ def fielddata(fields,format):
 
 
 @cat.command()
-def health():
+@click.option('-h', nargs=1, help=' A comma-separated list of column names')
+def health(h):
     try:
-        response = base.es.cat.health()
+        response = base.es.cat.health(h=h)
+        table = base.draw_table(response)
+    except Exception as e:
+        click.echo(e)
+    else:
+        click.echo(table.draw())
+
+
+@cat.command()
+@click.option('-h', nargs=1, help=' A comma-separated list of column names')
+def master(h):
+    try:
+        response = base.es.cat.master(h=h)
+        table = base.draw_table(response)
+    except Exception as e:
+        click.echo(e)
+    else:
+        click.echo(table.draw())
+
+
+@cat.command()
+@click.option('-h', nargs=1, help=' A comma-separated list of column names')
+def nodeattrs(h):
+    try:
+        response = base.es.cat.nodeattrs(h=h)
+        if not response:
+            click.echo("No attributes as of now")
+            return
+        table = base.draw_table(response)
+    except Exception as e:
+        click.echo(e)
+    else:
+        click.echo(table.draw())
+
+
+@cat.command()
+@click.option('-h', nargs=1, help=' A comma-separated list of column names')
+def nodes(h):
+    try:
+        response = base.es.cat.nodes(h)
+        if not response:
+            click.echo("No Nodes as of now")
+            return
         table = base.draw_table(response)
     except Exception as e:
         click.echo(e)
